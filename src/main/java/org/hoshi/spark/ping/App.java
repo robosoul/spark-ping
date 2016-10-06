fea6352a2700ef38eaa7dd30163e46f79e774422
robosoul <nomorestories@gmail.com>
2016-10-06 15:54:04 +0200

    Initial Commit

---
+++ src/main/java/org/hoshi/spark/ping/App.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright Vast 2016. All Rights Reserved.
+ *
+ * http://www.vast.com
+ */
+package org.hoshi.spark.ping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Luka Obradovic (luka@vast.com)
+ */
+public class App {
+    public static final Logger log = LoggerFactory.getLogger(App.class);
+}
